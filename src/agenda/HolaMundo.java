c8cde234b2443670a5a0351e395076ec839c0640
CHRISTIAN JORDI <CHRISTIAN JORDI@LAPTOP-79E59T1R>
2023-10-02 22:53:06 -0500

    Mi aporte es el HolaMundo Java en el archivo Agenda.java

--- src/agenda/HolaMundo.java
+++ src/agenda/HolaMundo.java
@@ -14,7 +14,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola desde JAVA");
+        System.out.println("Hola desde JAVA ");
                 
     }
     
